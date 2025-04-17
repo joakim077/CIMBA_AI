3a95a6f5cc2fe70a0e202fb2747314ddb3a3e065
joakim077 <122166749+joakim077@users.noreply.github.com>
2025-04-18 02:56:39 +0530

    Update UrlRequest.java

--- backend/src/main/java/com/cimba/backend/model/UrlRequest.java
+++ backend/src/main/java/com/cimba/backend/model/UrlRequest.java
@@ -3,6 +3,11 @@ package com.cimba.backend.model;
 public class UrlRequest {
     private String url;
 
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
